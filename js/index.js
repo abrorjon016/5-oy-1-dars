1add08aafd729b0dd717a527396853bf6062017b
abrorjon016 <ikromjonovabrorjon3@gmail.com>
2024-10-19 09:39:26 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,192 @@
+// forEach ----------------------------------
+
+// 1-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// arr.forEach(function (value) {
+//   console.log(value);
+// });
+
+// 2-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// arr.forEach(function (value) {
+//   console.log(value + 5);
+// });
+
+// 3-masala
+// let num = [1, 2, 3, 4, 5, 6];
+// num.forEach(function (value) {
+//   if (value % 2 == 0) {
+//     console.log(value);
+//   }
+// });
+
+// 4-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// arr.forEach(function (value, index,) {
+//   console.log(index);
+// });
+
+// 5-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// let arr2 = [];
+// arr.forEach(function (value) {
+//   arr2.push(value);
+// });
+// console.log(arr2);
+
+// map ------------------------------------------
+
+// 1-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// let res = arr.map(function (value) {
+//   return value * 2;
+// });
+// console.log(res);
+
+// 2-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// let res = arr.map(function (value) {
+//   return value * value;
+// });
+// console.log(res);
+
+// 3-masala
+// let bugatti = ["shiron", "veyron", "tourbillion"];
+// let res = bugatti.map(function (value) {
+//   return value.toUpperCase();
+// });
+// console.log(res);
+
+// 4-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// let res = arr.map(function (value) {
+//   return -value ;
+// });
+// console.log(res);
+
+// 5-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// let res = arr.map(function (value) {
+//   return value + 3;
+// });
+// console.log(res);
+
+// filter ----------------------------------
+
+// 1-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// let res = arr.map(function (value) {
+//   return value % 2 == 0;
+// });
+// console.log(res);
+
+// 2-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// let res = arr.map(function (value) {
+//   return > 0;
+// });
+// console.log(res);
+
+// 3-masala
+// let bugatti = ["shiron", "veyron", "tourbillion"];
+// let res = bugatti.map(function (value) {
+//   return  value.length > 5;
+// });
+// console.log(res)
+
+// 4-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// let res = arr.map(function (value) {
+//   return > 18;
+// });
+// console.log(res);
+
+// 5-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// let res = arr.map(function (value) {
+//   return value < 10;
+// });
+// console.log(res);
+
+// find --------------------------------------
+
+// 1-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// let res = arr.filter(function (value) {
+//   return value < 10;
+// });
+// console.log(res);
+
+// 2-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// let res = arr.filter(function (value) {
+//   return value > 0;
+// });
+// console.log(res);
+
+// 3-masala
+// let bugatti = ["shiron", "veyron", "tourbillion"];
+// let res = bugatti.filter(function (value) {
+//   return  value.length > 4;
+// });
+// console.log(res)
+
+// 4-masala
+// let bugatti =  [1, 2, 3, 4, 5, 6];
+// let res = bugatti.filter(function (value) {
+//   return  value % 2 == 0;
+// });
+// console.log(res)
+
+// 5-masala
+// let bugatti = [1, 2, 3, 4, 5, 6];
+// let res = bugatti.filter(function (value) {
+//   return  value >= 0;
+// });
+// console.log(res)
+
+// some/every----------------------------------
+
+// 1-masala
+// let bugatti = [1, 2, 3, 4, 5, 6];
+// let res = bugatti.some(function (value) {
+//   return value < 0;
+// });
+// console.log(res);
+
+// 2-masala
+// let bugatti = [1, 2, 3, 4, 5, 6];
+// let res = bugatti.some(function (value) {
+//   return value % 2 == 0;
+// });
+// console.log(res);
+
+// 3-masala
+// let bugatti = ["shiron", "veyron", "tourbillion"];
+// let res = bugatti.some(function (value) {
+//   return  value == "veyron";
+// });
+// console.log(res)
+
+// every -------------------------------------------
+
+// 4-masala
+// let bugatti = [1, 2, 3, 4, 5, 6];
+// let res = bugatti.every(function (value) {
+//   return value > 0;
+// });
+// console.log(res);
+
+// 5-masala
+// let bugatti = ["shiron", "veyron", "tourbillion"];
+// let res = bugatti.every(function (value) {
+//   return value === "String";
+// });
+// console.log(res);
+
+// 6-masala
+// let bugatti = [1, 2, 3, 4, 5, 6];
+// let res = bugatti.every(function (value) {
+//   return value < 100;
+// });
+// console.log(res);
